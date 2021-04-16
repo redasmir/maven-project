cef1c21cae28ad2e8e0d43912f26263f7f401527
redasmir <81695891+redasmir@users.noreply.github.com>
2021-04-16 14:55:57 +0200

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,7 +13,6 @@ public class Greeter {
   *@return greeting string
   */
   
-  //comment
 
   public final String greet(final String someone) {
     return String.format("Hello babacar, %s!", someone);
